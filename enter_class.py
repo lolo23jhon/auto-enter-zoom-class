f982517267b899be97e6dbbe761a3b54799e0e3e
Pedro Escoboza <psebastian01@hotmail.com>
2020-08-08 20:44:30 -0700

    Added tables and new courses.

--- enter_class.py
+++ enter_class.py
@@ -2,6 +2,7 @@ import json
 import os
 from datetime import datetime
 from time import sleep
+from terminaltables import AsciiTable
 
 
 DAYS_WEEK = {
@@ -48,10 +49,10 @@ def is_it_time(t_start, t_finish, t_datetime=datetime.now()):
     return now_int >= start_int and now_int <= finish_int
 
 
-def main():
-    DELAY_SECONDS = 1.2
+def enter_class(course_info_json):
+    DELAY_SECONDS = 1.5
     data = dict()
-    with open("courses.json") as f:
+    with open(course_info_json) as f:
         data = json.load(f)
 
     now = datetime.now()
@@ -60,21 +61,35 @@ def main():
 
     for course_name, course in data["courses"].items():
         for dow, time_range in course["sessions"].items():
+            
             if dow == weekday:
                 is_time = is_it_time(
                     time_range["start"], time_range["finish"], t_datetime=now)
+            
                 if is_time:
+                    table_data = [["Course", "Start", "Finish"],
+                                    [course_name, time_range["start"], time_range["finish"]]]
+                    table = AsciiTable(table_data)
+                    print(table.table)
+                    """
                     print("Joining your class {}.".format(course_name))
                     print("Start:\t{}\nFinish:\t{}".format(
                         time_range["start"], time_range["finish"]))
-                    if DELAY_SECONDS > 0:
-                        sleep(DELAY_SECONDS)
+                    """
                     cmd = "start {}".format(course["url"])
                     os.system(cmd)
+                    
+                    if DELAY_SECONDS > 0:
+                        sleep(DELAY_SECONDS)
+                                        
                     return
+    print("==============================================")
     print("You do not have class right now.")
+    print("==============================================\n")
     input("Press Enter to exit.")
 
 
 if __name__ == "__main__":
-    main()
+    enter_class("courses_1.json")
+
+# TODO: Change zoom urls in json files.
